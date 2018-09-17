80537cb3758a4155ff2bf58071136546434e6041
kangyang <kangyang@travelsky.com>
2018-09-17 16:59:26 +0800

    提交redis配置

--- src/main/java/com/zyh/controller/user/UserLoginController.java
+++ src/main/java/com/zyh/controller/user/UserLoginController.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,24 +12,35 @@ import org.springframework.web.bind.annotation.RestController;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.zyh.entity.common.ResponeToWeb;
 import com.zyh.entity.user.ZyhUser;
+import com.zyh.service.user.ILoginService;
 
 @RestController
 public class UserLoginController {
-	
+
 	private Logger log = Logger.getLogger("error");
 	
-	@RequestMapping("/test.act")
-	public ResponeToWeb login(@RequestBody String json){
+	@Autowired
+	private ILoginService loginService;
+
+	@RequestMapping("/login.act")
+	public ResponeToWeb login(@RequestBody String json) {
 		ResponeToWeb responeToWeb = new ResponeToWeb();
 		ObjectMapper om = new ObjectMapper();
 		Map<String, Object> map = new HashMap<String, Object>();
 		try {
 			ZyhUser user = om.readValue(json, ZyhUser.class);
-			responeToWeb.setMsg("登录成功");
-			responeToWeb.setSuccess(true);
-			responeToWeb.setValue(map);
+			if (null != user && null != user.getType() && !"".equals(user.getType())) {
+				user = loginService.loginAndRegister(user);
+				map.put("result", user);
+				responeToWeb.setMsg("登录成功");
+				responeToWeb.setSuccess(true);
+				responeToWeb.setValue(map);
+			} else {
+				responeToWeb.setMsg("登录失败,信息缺失");
+				responeToWeb.setSuccess(false);
+			}
 		} catch (Exception e) {
-			log.error("登录失败："+e.getMessage());
+			log.error("登录失败：" + e.getMessage());
 			responeToWeb.setMsg("登录失败");
 			responeToWeb.setSuccess(false);
 		}
